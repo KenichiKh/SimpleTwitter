a9f15914a72405b19fdd8a59f1ef7a59e3494032
KenichiKh <kenichishirohama72@gmail.com>
2022-03-18 23:41:37 -0400

    Assignment 3

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,15 +1,26 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
+    lateinit var client: TwitterClient
+    lateinit var tweetCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +28,29 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btnTweet)
+        client = TwitterApplication.getRestClient(this)
 
+        tweetCount = findViewById(R.id.count_tweet)
+//        tILayout = findViewById(R.id.tILayout)
+
+        etCompose.addTextChangedListener(object : TextWatcher{
+            val tweetContent = etCompose.text.toString()
+//            val tweetCounter = tweetContent.length
+//            val maxChar = 280
+//            val charLeft = maxChar
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                if (tweetContent.isNotEmpty() && tweetContent.length <= 280){
+//                    btnTweet.isEnabled = true
+//                } else { btnTweet.isEnabled = false}
+                btnTweet.isEnabled = tweetContent.isNotEmpty() && tweetContent.length <= 280
+                tweetCount.text = (280 - etCompose.text.toString().trim().length).toString()
+                }
+
+
+        })
         // Handling the user's click on the tweet button
         btnTweet.setOnClickListener{
 
@@ -26,20 +59,42 @@ class ComposeActivity : AppCompatActivity() {
 
             // 1.Make sure the tweet isn't empty
             if (tweetContent.isEmpty()){
-                Toast.makeText(
-                    this,
-                    "Empty tweets not allowed!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Empty tweets not allowed!", Toast.LENGTH_SHORT).show()
                 //Look into displaying SnackBar message
-            }
+            } else
             // 2. Make sure the tweet is under character count
-            if (tweetContent.length > 140){
+                if (tweetContent.length > 280){
                 Toast.makeText(
                     this,
-                    "Tweet is too long! Limit is 140 characters", Toast.LENGTH_SHORT).show()
+                    "Tweet is too long! Limit is 280 characters", Toast.LENGTH_SHORT).show()
+
             } else {
-                // TODO: Make an api call to Twitter to publish tweet
-                Toast.makeText(this, tweetContent, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Tweet Success!", Toast.LENGTH_SHORT).show()
+                client.publishTweet(tweetContent, object : JsonHttpResponseHandler(){
+
+                    override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                        Log.i(TAG, "Successfully published tweet!")
+
+                        val tweet = Tweet.fromJson(json.jsonObject)
+                        val intent = Intent()
+                        intent.putExtra("tweet", tweet)
+                        setResult(RESULT_OK, intent)
+                        finish()
+                    }
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+                        Log.e(TAG, "Failed to publish tweet", throwable)
+                    }
+                })
             }
         }
+    }
+//    private fun textFocusListener
+    companion object{
+        val TAG = "ComposeActivity"
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -13,6 +13,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import okhttp3.Request
 import org.json.JSONException
 
 class TimelineActivity : AppCompatActivity() {
@@ -58,15 +59,34 @@ class TimelineActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
     //Handles clicks on menu item
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.compose){
             // Navigate to compose screen
             val intent = Intent(this, ComposeActivity::class.java)
-             startActivity(intent)
+             startActivityForResult(intent, REQUEST_CODE)
 
     }
         return super.onOptionsItemSelected(item)
+    }
+
+    //This method is called when we come back from ComposeActivity
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        //REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+            //Get data from our intent (our tweet)
+            val tweet = data?.getParcelableExtra<Tweet>("tweet") as Tweet
+            //Update timeline
+            //Modifying the data source of tweets
+            tweets.add(0, tweet)
+
+            //Update adapter
+            adapter.notifyItemInserted(0)
+            rvTweets.smoothScrollToPosition(0)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     fun populateHomeTimeline(){
@@ -103,6 +123,7 @@ class TimelineActivity : AppCompatActivity() {
     }
     companion object {
         val TAG = "TimelineActivity"
+        val REQUEST_CODE = 10
     }
 
 }
